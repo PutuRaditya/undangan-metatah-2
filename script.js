6eb80803b763b4c1c12d194cc0ae57da0d077a9d
Putu Raditya Perdana <raditperdana20@gmail.com>
2025-06-11 14:57:29 +0800

    Menambah Konten Mohon Doa Restu - Menambahkan Form Doa Restu yang terintegrasi dengan Google spreadsheet - Pembuatan tahap pertama

---
+++ script.js
@@ -0,0 +1,16 @@
+{/* <script>
+      window.addEventListener("load", function () {
+        const form = document.getElementById("doaForm");
+        form.addEventListener("submit", function (e) {
+          e.preventDefault();
+          const data = new FormData(form);
+          const action = e.target.action;
+          fetch(action, {
+            method: "POST",
+            body: data,
+          }).then(() => {
+            alert("Konfirmasi Kehadiran Berhasil terkirim!");
+          });
+        });
+      });
+    </script> */}
